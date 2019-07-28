7b9fcefdcfa1f6e82a075be39553235b8000cbbf
shahara21-meet <shahara21@meet.mit.edu>
2019-07-28 18:26:34 +0300

    Stopping here

--- lab_6.py
+++ lab_6.py
@@ -1,7 +1,11 @@
-import turtle
-x=0
-while x<300:
-    y=x*x
-    turtle.goto(x,y)
-    x=x+100
-turtle.mainloop()
+def add(start,end,n):
+    total = 0
+    for number in range(start, end + n):
+        num=num+
+        print(num)
+        total = total + number
+    return total
+
+x=add(1,10,2)
+print(x)
+
